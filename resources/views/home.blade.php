9b0d1b869e5fe64574db67619c137002a0c6824d
hansarwa2406 <kuronokameraman@gmail.com>
2021-10-16 08:06:26 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+
+@section('container')
+    {{-- <h1>Halaman Home</h1> --}}
+    <div class="row justify-content-center">
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/HqAMb6kqlLs?&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+@endsection
