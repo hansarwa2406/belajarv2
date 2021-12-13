e749f3e299772e686c1d94b8cce28fced637be2f
hansarwa2406 <kuronokameraman@gmail.com>
2021-12-13 22:16:53 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -53,41 +53,5 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Post::factory(28)->create();
-
-        // Post::create([
-        //     'title' => 'Judul Pertama',
-        //     'slug' => 'judul-pertama',
-        //     'excerpt' => 'Lorem 3rd, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga?',
-        //     'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga? Quae veritatis odio molestias cumque et culpa ratione qui deleniti, optio excepturi mollitia consectetur quasi hic dolor obcaecati temporibus dolores nobis vitae ipsa quod nulla enim repellendus eius magnam? Enim illum facilis sed perferendis vitae et maiores aperiam ipsa, odit, aspernatur quos aliquam atque tempore eligendi recusandae id!</p> <p>Tempora, perferendis repellat asperiores harum provident illum illo quae maxime, aperiam, unde voluptatem ab a voluptates debitis? Dignissimos rerum hic totam consequuntur laudantium pariatur ut at ipsam ab praesentium nobis, facere illum tenetur.</p> <p>Itaque vero recusandae dolor quas vitae veritatis consequatur minima possimus reiciendis qui sequi ex, eius iure deleniti eaque ab sint a autem magni laboriosam. Qui officia aspernatur beatae molestiae aliquid enim doloribus totam laborum soluta accusamus, voluptatem quod inventore dolorem? Iure id dicta temporibus.</p>',
-        //     'category_id' => 1,
-        //     'user_id' => 1
-        // ]);
-        
-        // Post::create([
-        //     'title' => 'Judul Kedua',
-        //     'slug' => 'judul-kedua',
-        //     'excerpt' => 'Lorem 3rd, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga?',
-        //     'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga? Quae veritatis odio molestias cumque et culpa ratione qui deleniti, optio excepturi mollitia consectetur quasi hic dolor obcaecati temporibus dolores nobis vitae ipsa quod nulla enim repellendus eius magnam? Enim illum facilis sed perferendis vitae et maiores aperiam ipsa, odit, aspernatur quos aliquam atque tempore eligendi recusandae id!</p> <p>Tempora, perferendis repellat asperiores harum provident illum illo quae maxime, aperiam, unde voluptatem ab a voluptates debitis? Dignissimos rerum hic totam consequuntur laudantium pariatur ut at ipsam ab praesentium nobis, facere illum tenetur.</p> <p>Itaque vero recusandae dolor quas vitae veritatis consequatur minima possimus reiciendis qui sequi ex, eius iure deleniti eaque ab sint a autem magni laboriosam. Qui officia aspernatur beatae molestiae aliquid enim doloribus totam laborum soluta accusamus, voluptatem quod inventore dolorem? Iure id dicta temporibus.</p>',
-        //     'category_id' => 1,
-        //     'user_id' => 1
-        // ]);
-        
-        // Post::create([
-        //     'title' => 'Judul Ketiga',
-        //     'slug' => 'judul-ketiga',
-        //     'excerpt' => 'Lorem 3rd, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga?',
-        //     'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga? Quae veritatis odio molestias cumque et culpa ratione qui deleniti, optio excepturi mollitia consectetur quasi hic dolor obcaecati temporibus dolores nobis vitae ipsa quod nulla enim repellendus eius magnam? Enim illum facilis sed perferendis vitae et maiores aperiam ipsa, odit, aspernatur quos aliquam atque tempore eligendi recusandae id!</p> <p>Tempora, perferendis repellat asperiores harum provident illum illo quae maxime, aperiam, unde voluptatem ab a voluptates debitis? Dignissimos rerum hic totam consequuntur laudantium pariatur ut at ipsam ab praesentium nobis, facere illum tenetur.</p> <p>Itaque vero recusandae dolor quas vitae veritatis consequatur minima possimus reiciendis qui sequi ex, eius iure deleniti eaque ab sint a autem magni laboriosam. Qui officia aspernatur beatae molestiae aliquid enim doloribus totam laborum soluta accusamus, voluptatem quod inventore dolorem? Iure id dicta temporibus.</p>',
-        //     'category_id' => 2,
-        //     'user_id' => 1
-        // ]);
-        
-        // Post::create([
-        //     'title' => 'Judul Keempat',
-        //     'slug' => 'judul-keempat',
-        //     'excerpt' => 'Lorem 3rd, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga?',
-        //     'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad velit asperiores quo blanditiis aliquam! Sed ad labore nemo mollitia esse voluptatum nostrum sunt, veniam fuga? Quae veritatis odio molestias cumque et culpa ratione qui deleniti, optio excepturi mollitia consectetur quasi hic dolor obcaecati temporibus dolores nobis vitae ipsa quod nulla enim repellendus eius magnam? Enim illum facilis sed perferendis vitae et maiores aperiam ipsa, odit, aspernatur quos aliquam atque tempore eligendi recusandae id!</p> <p>Tempora, perferendis repellat asperiores harum provident illum illo quae maxime, aperiam, unde voluptatem ab a voluptates debitis? Dignissimos rerum hic totam consequuntur laudantium pariatur ut at ipsam ab praesentium nobis, facere illum tenetur.</p> <p>Itaque vero recusandae dolor quas vitae veritatis consequatur minima possimus reiciendis qui sequi ex, eius iure deleniti eaque ab sint a autem magni laboriosam. Qui officia aspernatur beatae molestiae aliquid enim doloribus totam laborum soluta accusamus, voluptatem quod inventore dolorem? Iure id dicta temporibus.</p>',
-        //     'category_id' => 2,
-        //     'user_id' => 2
-        // ]);
     }
 }
