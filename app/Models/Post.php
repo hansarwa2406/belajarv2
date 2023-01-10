7912b8d08ccee42c5388acbe8353a20662cd3c12
hansarwa2406 <kuronokameraman@gmail.com>
2023-01-10 08:57:51 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,24 +24,28 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, function($query, $search) {
-            return $query->where(function($query) use ($search) {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where(function ($query) use ($search) {
                 $query->where('title', 'like', '%' . $search . '%')
                     ->orWhere('body', 'like', '%' . $search . '%');
             });
         });
 
-        $query->when($filters['category'] ?? false, function($query, $category) {
-            return $query->whereHas('category', function($query) use ($category) {
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
                 $query->where('slug', $category);
             });
         });
 
-        $query->when($filters['author'] ?? false, fn($query, $author) => 
-            $query->whereHas('author', fn($query) =>
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
                 $query->where('username', $author)
             )
-    );
+        );
     }
 
     public function category()
